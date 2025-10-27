c356263dd2254481011ae78233bdaf2c19bb5bec
Srikar Bingi <60936704+SrikarBingi@users.noreply.github.com>
2025-10-27 13:03:39 +0530

    day 3 method overriding

--- Overriding/Dog.java
+++ Overriding/Dog.java
@@ -2,6 +2,7 @@ package Overriding;
 
 public class Dog extends Animal{
     void makeSound(){
+        super.makeSound(); // prints super class method
         System.out.println("Dog barks");
     }
 }
