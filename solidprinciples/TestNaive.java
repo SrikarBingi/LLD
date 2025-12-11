c6f34037e0bd036dbb6c33884ca6ed48d06776c4
Srikar Bingi <60936704+SrikarBingi@users.noreply.github.com>
2025-12-11 11:13:44 +0530

    applied SRP

--- solidprinciples/TestNaive.java
+++ solidprinciples/TestNaive.java
@@ -21,3 +21,32 @@ public class TestNaive {
     }
 }
 
+/*
+PROBLEMS with Naive Approach:
+1. If-else chains 
+2. to add new bird  fly(), makeSound() should be modifies
+3. Penguin, Ostrich exposes fly() method (confusing)
+4. Crow, Sparrow fly in same way but not mentioned in code
+5. Testing becomes tough
+6. Code duplication
+
+we have to apply solid principles to overcome these
+*/
+
+
+// SRP
+
+/*
+SRP Definition:
+"A class should have only ONE reason to change"
+"A class should have only one responsibility."
+
+When should we apply SRP?
+- When a class is doing many different things
+- When a class has multiple reasons to change
+- When the methods inside a class are not related to each other
+
+What we will do now:
+- Create a separate class for each bird type
+- Each class will handle its own behavior
+*/
